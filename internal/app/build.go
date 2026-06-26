97f35133738cb0d530ee1b1f6d9599702e0fb1be
mshegolev <mshegolev@gmail.com>
2026-06-26 18:43:56 +0400

    Make bqa build generate knowledge artifacts only

--- internal/app/build.go
+++ internal/app/build.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	fsadapter "github.com/mshegolev/bqa-os/internal/adapters/fs"
-	coreartifacts "github.com/mshegolev/bqa-os/internal/core/artifacts"
 	coreknowledge "github.com/mshegolev/bqa-os/internal/core/knowledge"
 	"github.com/spf13/cobra"
 )
@@ -27,17 +26,9 @@ func buildCmd() *cobra.Command {
 				return err
 			}
 
-			artifactUC := coreartifacts.UseCase{Writer: store}
-			artifactResult, err := artifactUC.Run(ctx)
-			if err != nil {
-				return err
-			}
-
 			fmt.Printf("Sessions processed: %d\n", knowledgeResult.SessionsProcessed)
 			fmt.Printf("Knowledge artifacts created: %d\n", knowledgeResult.ArtifactsCreated)
-			fmt.Printf("BQA artifacts created: %d\n", artifactResult.ArtifactsCreated)
 			fmt.Printf("Knowledge dir: %s\n", knowledgeDir)
-			fmt.Println("Generated dirs: .bqa/skills .bqa/agents .bqa/workflows .bqa/registry")
 			return nil
 		},
 	}
