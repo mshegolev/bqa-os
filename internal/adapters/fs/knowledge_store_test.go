f6ae567f8122471e9f84a4d85c7294135fd4930f
mshegolev <mshegolev@gmail.com>
2026-06-26 19:15:30 +0400

    Add FS knowledge writer test

--- internal/adapters/fs/knowledge_store_test.go
+++ internal/adapters/fs/knowledge_store_test.go
@@ -1,5 +1,27 @@
 package fs
 
-import "testing"
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"testing"
+)
 
-func TestKnowledgeStoreCompiles(t *testing.T) {}
+func TestKnowledgeStoreWritesYamlArtifact(t *testing.T) {
+	dir := t.TempDir()
+	knowledgeDir := filepath.Join(dir, "knowledge")
+	store := KnowledgeStore{KnowledgeDir: knowledgeDir}
+
+	content := "etl_patterns:\n  []\n"
+	if err := store.WriteKnowledgeArtifact(context.Background(), "etl_patterns.yaml", content); err != nil {
+		t.Fatalf("WriteKnowledgeArtifact returned error: %v", err)
+	}
+
+	data, err := os.ReadFile(filepath.Join(knowledgeDir, "etl_patterns.yaml"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(data) != content {
+		t.Fatalf("unexpected content: %q", string(data))
+	}
+}
