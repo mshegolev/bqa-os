191f11795fe2a325761d6cc48755d3182d56b5c5
mshegolev <mshegolev@gmail.com>
2026-06-26 18:42:41 +0400

    Refine knowledge extractor domain models

--- internal/core/knowledge/models.go
+++ internal/core/knowledge/models.go
@@ -1,5 +1,26 @@
 package knowledge
 
+type Domain string
+
+const (
+	DomainETL         Domain = "etl"
+	DomainGraphQL     Domain = "graphql"
+	DomainAPI         Domain = "api"
+	DomainDataQuality Domain = "data_quality"
+	DomainBugs        Domain = "bugs"
+	DomainPrompts     Domain = "prompts"
+)
+
+var DefaultArtifactFilenames = []string{
+	"etl_patterns.yaml",
+	"graphql_patterns.yaml",
+	"api_patterns.yaml",
+	"data_quality_patterns.yaml",
+	"common_bugs.yaml",
+	"successful_prompts.yaml",
+	"project_profile.yaml",
+}
+
 type Artifact struct {
 	Filename string
 	Content  string
@@ -7,7 +28,8 @@ type Artifact struct {
 
 type Finding struct {
 	Name       string
-	Domain     string
+	Domain     Domain
+	Keywords   []string
 	Evidence   string
 	SourcePath string
 }
@@ -19,11 +41,11 @@ type Result struct {
 }
 
 type ProjectProfile struct {
-	Sessions       int
-	ETLSignals     int
-	GraphQLSignals int
-	APISignals     int
-	DQSignals      int
-	DroidSignals   int
-	RuntimeSignals int
+	Sessions           int
+	ETLFindings        int
+	GraphQLFindings    int
+	APIFindings        int
+	DataQualityFindings int
+	BugFindings        int
+	PromptFindings     int
 }
