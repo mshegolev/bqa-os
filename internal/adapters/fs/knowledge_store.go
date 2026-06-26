71efbbc164eb97c060e09d21b5ce3b1374608204
mshegolev <mshegolev@gmail.com>
2026-06-26 16:33:59 +0400

    Add fs knowledge store adapter

---
+++ internal/adapters/fs/knowledge_store.go
@@ -0,0 +1,73 @@
+package fs
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"path/filepath"
+
+	"github.com/mshegolev/bqa-os/internal/ports"
+)
+
+type KnowledgeStore struct {
+	SessionBaseDir string
+	KnowledgeDir   string
+}
+
+func (s KnowledgeStore) LoadSessionIndex(ctx context.Context) (ports.SessionIndex, error) {
+	select {
+	case <-ctx.Done():
+		return ports.SessionIndex{}, ctx.Err()
+	default:
+	}
+	path := filepath.Join(s.sessionBase(), "index.json")
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return ports.SessionIndex{}, err
+	}
+	var index ports.SessionIndex
+	if err := json.Unmarshal(data, &index); err != nil {
+		return ports.SessionIndex{}, err
+	}
+	return index, nil
+}
+
+func (s KnowledgeStore) ReadNormalizedSession(ctx context.Context, path string) (string, error) {
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	default:
+	}
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+func (s KnowledgeStore) WriteKnowledgeArtifact(ctx context.Context, filename string, content string) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+	}
+	path := filepath.Join(s.knowledgeDir(), filename)
+	if err := os.MkdirAll(filepath.Dir(path), 0o755); err != nil {
+		return err
+	}
+	return os.WriteFile(path, []byte(content), 0o600)
+}
+
+func (s KnowledgeStore) sessionBase() string {
+	if s.SessionBaseDir == "" {
+		return ".bqa/input/sessions"
+	}
+	return s.SessionBaseDir
+}
+
+func (s KnowledgeStore) knowledgeDir() string {
+	if s.KnowledgeDir == "" {
+		return ".bqa/knowledge"
+	}
+	return s.KnowledgeDir
+}
